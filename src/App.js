50731ea7e9c3ee8d3b0db8104b0f39704f6053ff
이정현 <wjd1gus2@gmail.com>
2025-10-21 18:32:36 +0900

    feat(app): add function to check user input name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Random, Console } from '@woowacourse/mission-utils';
 
 const NAME_PLACEHOLDER = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+const PATTERN_NAME = /^[가-힣a-zA-Z0-9,]+$/
 
 class App {
   async run() {
@@ -10,6 +11,14 @@ class App {
 
 async function getUserInput(placeholder) {
   return await Console.readLineAsync(placeholder)
+}
+
+export function checkUserInputName(userInputName) {
+  userInputName.replace(/\s+/g, '');
+  if (!userInputName) throw new Error('[ERROR] 이름을 입력해주세요.');
+  if (!PATTERN_NAME.test(userInputName)) throw new Error('[ERROR] 이름은 쉼표(,) 기준으로 구분해주세요.');
+  const names = userInputName.split(',');
+  if (names.some((name) => name.length > 5)) throw new Error('[ERROR] 이름은 5자 이하로 입력해주세요.');
 }
 
 export default App;
--- src/test.js
+++ src/test.js
@@ -1,19 +1,20 @@
-
+import { checkUserInputName } from './App.js';
 
 describe('userInputName 유효성 검사', () => {
     beforeAll(() => console.log('테스트 시작'));
+    afterAll(() => console.log('테스트 종료'));
 
     test('사용자가 이름 입력시, 빈 문자열일 경우 에러를 발생시킨다.', () =>{
-        expect(checkUserInputName('')).toThrow('[ERROR]');
+        expect(() => checkUserInputName("")).toThrow('[ERROR]');
     });
 
     test('사용자가 이름 입력시, 구분자 (,) 외에 다른 특수문자가 포함되어 있을 경우 에러를 발생시킨다.', () => {
-        expect(checkUserInputName('pobi.woni')).toThrow('[ERROR]');
+        expect(() => checkUserInputName('pobi.woni')).toThrow('[ERROR]');
     });
 
     test('사용자가 이름 입력시, 이름이 6글자 이상일 경우 에러를 발생시킨다.', () => {
-        expect(checkUserInputName('pongki')).toThrow('[ERROR]');
+        expect(() => checkUserInputName('pongki')).toThrow('[ERROR]');
     });
 
-    afterAll(() => console.log('테스트 종료'));
+    
 });

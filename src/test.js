453d8ead4ea9e4d8571026b01639bf899e48c592
이정현 <wjd1gus2@gmail.com>
2025-10-21 17:04:33 +0900

    test(app): add test for checkUserInputName function

---
+++ src/test.js
@@ -0,0 +1,19 @@
+
+
+describe('userInputName 유효성 검사', () => {
+    beforeAll(() => console.log('테스트 시작'));
+
+    test('사용자가 이름 입력시, 빈 문자열일 경우 에러를 발생시킨다.', () =>{
+        expect(checkUserInputName('')).toThrow('[ERROR]');
+    });
+
+    test('사용자가 이름 입력시, 구분자 (,) 외에 다른 특수문자가 포함되어 있을 경우 에러를 발생시킨다.', () => {
+        expect(checkUserInputName('pobi.woni')).toThrow('[ERROR]');
+    });
+
+    test('사용자가 이름 입력시, 이름이 6글자 이상일 경우 에러를 발생시킨다.', () => {
+        expect(checkUserInputName('pongki')).toThrow('[ERROR]');
+    });
+
+    afterAll(() => console.log('테스트 종료'));
+});

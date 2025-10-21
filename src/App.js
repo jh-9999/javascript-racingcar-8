9ba633c6be92764b98b56b763a140c91268186ca
이정현 <wjd1gus2@gmail.com>
2025-10-21 17:19:53 +0900

    feat(app): add function to get user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
+const NAME_PLACEHOLDER = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+
 class App {
-  async run() {}
+  async run() {
+    const userInputName = await getUserInput(NAME_PLACEHOLDER)
+  }
+}
+
+async function getUserInput(placeholder) {
+  return await Console.readLineAsync(placeholder)
 }
 
 export default App;

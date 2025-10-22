1dca339ef4dd2fd0a73b8a93ab7dd55d052936db
이정현 <wjd1gus2@gmail.com>
2025-10-22 11:12:40 +0900

    feat(app): add function to display error message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { Random, Console } from '@woowacourse/mission-utils';
 
 const NAME_PLACEHOLDER = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
 const PATTERN_NAME = /^[가-힣a-zA-Z0-9,]+$/
+const ERROR_MESSAGE = '[ERROR]';
+const ERROR_MESSAGE_EMPTY_NAME = '이름을 입력해주세요.';
+const ERROR_MESSAGE_INVALID_DELIMITER = '이름은 쉼표(,) 기준으로 구분해주세요.';
+const ERROR_MESSAGE_NAME_LENGTH = '이름은 5자 이하로 입력해주세요.';
 
 class App {
   async run() {
@@ -15,10 +19,14 @@ async function getUserInput(placeholder) {
 
 export function checkUserInputName(userInputName) {
   userInputName.replace(/\s+/g, '');
-  if (!userInputName) throw new Error('[ERROR] 이름을 입력해주세요.');
-  if (!PATTERN_NAME.test(userInputName)) throw new Error('[ERROR] 이름은 쉼표(,) 기준으로 구분해주세요.');
+  if (!userInputName) errorMessage(ERROR_MESSAGE_EMPTY_NAME)
+  if (!PATTERN_NAME.test(userInputName)) errorMessage(ERROR_MESSAGE_INVALID_DELIMITER)
   const names = userInputName.split(',');
-  if (names.some((name) => name.length > 5)) throw new Error('[ERROR] 이름은 5자 이하로 입력해주세요.');
+  if (names.some((name) => name.length > 5)) errorMessage(ERROR_MESSAGE_NAME_LENGTH)
+}
+
+function errorMessage(message) {
+  throw new Error(`${ERROR_MESSAGE} ${message}`);
 }
 
 export default App;
